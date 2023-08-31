13298ce10d50a3ba626d8928f926195d72fcbe14
prafailov9 <plamen.rafailov@yahoo.com>
2023-08-31 03:06:34 +0300

    Refactoring

--- persistence/src/main/java/com/project/app/coredb/PreparedStatementBinder.java
+++ persistence/src/main/java/com/project/app/coredb/PreparedStatementBinder.java
@@ -3,6 +3,7 @@ package com.project.app.coredb;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementBinder {
 
   void bind(PreparedStatement preparedStatement) throws SQLException;
